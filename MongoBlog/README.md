bd03602b476a3baadaee0e20c418059de2d1a045
unknown <jmzombe@RD6UW-91882I.infosec.tycho.ncsc.mil>
2017-10-06 15:26:09 -0400

    initial push

---
+++ MongoBlog/README.md
@@ -0,0 +1,1 @@
+Mongo Blob Application
---
+++ MongoBlog/src/main/java/com/jmzombe/blog/Helpers.java
@@ -0,0 +1,29 @@
+/* Mongodb course - M101J
+ * Copyright (c) 2015
+ */
+package com.jmzombe.blog;
+
+import java.io.StringWriter;
+
+import org.bson.Document;
+import org.bson.codecs.DocumentCodec;
+import org.bson.codecs.EncoderContext;
+import org.bson.json.JsonMode;
+import org.bson.json.JsonWriter;
+import org.bson.json.JsonWriterSettings;
+
+/**
+ * TODO Add element description, main objective and relations
+ *
+ * @author wilson
+ *
+ */
+public class Helpers {
+
+    public static void printJson(Document document) {
+        JsonWriter jsonWriter = new JsonWriter(new StringWriter(), new JsonWriterSettings(JsonMode.SHELL, false));
+        new DocumentCodec().encode(jsonWriter, document, EncoderContext.builder().isEncodingCollectibleDocument(true).build());
+        System.out.println(jsonWriter.getWriter());
+        System.out.flush();
+    }
+}
